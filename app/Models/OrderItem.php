25d18c888ad5c49b19b9a2bb9fc5c042d928b7f4
Rupam058 <rupam.irp@gmail.com>
2025-06-18 14:52:10 +0530

    1 - Models and Migrations

---
+++ app/Models/OrderItem.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OrderItem extends Model {
+    protected $fillable = [
+        'order_by',
+        'product_id',
+        'quantity',
+        'unit_amout',
+        'total_amount',
+    ];
+
+    public function order() {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function product() {
+        return $this->belongsTo(Product::class);
+    }
+}
